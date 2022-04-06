b209cbff06dcc56b64c9ddec8e9eb2a57656fa7a
Santiago Neira <santiagoneira52@gmail.com>
2022-04-06 06:03:42 +0000

    function getenv ya separada

--- 4-print_environ.c
+++ 4-print_environ.c
@@ -2,17 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 
+char *_getenv(const char *var, char **env)
+{	
+	unsigned int i = 0;
+	unsigned int len = strlen(var);
+	
+	if (!env || ! *var || strchr(var,'='))
+		return (NULL);
+	else
+	while (env[i] && (env[i][len] != '=' || strncmp(var, env[i], len)))
+		i++;
+
+	return (env[i]) ? (env[i] + len + 1) : (NULL);
+}
+
+
 int main(int __attribute__((unused)) ac, char __attribute__((unused)) **av, char **env)
 {
 	char *var = "PATH";	
-	unsigned int i = 0;
-        unsigned int len = strlen(var);
+	char *path;
 
-	if (!env || !*var || strchr(var,'='))
-	printf("error"); /*NULL;*/
-	while ( env[i] && (env[i][len] != '=' || strncmp(var, env[i], len)) )
-		i++;
-	printf("%s\n", (env[i]) ? env[i] + len + 1 : NULL);
+	path = _getenv(var, env);
+
+	printf("%s\n", path);
 
 	return(0);
 }
