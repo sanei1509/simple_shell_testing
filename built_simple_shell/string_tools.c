3754ec01ae828eb47a373b89715a27151feca689
Santiago Neira <santiagoneira52@gmail.com>
2022-04-10 23:41:26 -0300

    fixing more betty

--- built_simple_shell/string_tools.c
+++ built_simple_shell/string_tools.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- *_putchar - writes the character c to stdout
- * @c: The character to print.
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
 **_strcat - function tha concatenate two strings
 * @dest : place where append de src string
 * @src: place of the string for append
